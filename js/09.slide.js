40b5435a45a5e13343466270eebcdef7fa994c13
SangYong-Jeong <mmaf@naver.com>
2021-08-25 18:00:11 +0900

    jjal repeat

--- js/09.slide.js
+++ js/09.slide.js
@@ -4,8 +4,8 @@ var idx = 0, last, prev, next;
 
 /************* user function *************/
 function init () {
-	$slide = $('.slide-wrap .slide').slice();
-	last = $slide.length - 1;
+	$slide = $('.slide-wrap .slide').slice(); // deepcopy
+	last = $slide.length - 1; // 5
 	slideInit();
 }
 
@@ -13,7 +13,7 @@ function slideInit() {
 	$('.slide-wrap').css('left', '-100%').empty();
 	prev = (idx === 0) ? last : idx - 1;
 	next = (idx === last) ? 0 : idx + 1;
-		$($slide[idx]).clone().appendTo('.slide-wrap');
+		$($slide[idx]).clone().appendTo('.slide-wrap'); // 그냥 배열처럼 값을 부르면 자바스크립트 객체가 된다 $slide가 jQuery 객체이기 때문이다. (그냥 배열 아님)
 		$($slide[prev]).clone().prependTo('.slide-wrap');
 		$($slide[next]).clone().appendTo('.slide-wrap');
 }
