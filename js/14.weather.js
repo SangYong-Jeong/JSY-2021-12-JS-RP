769f78dc66dbbe20aa6097c43d10e5d10e4d9afb
SangYong-Jeong <mmaf@naver.com>
2021-11-04 23:13:58 +0900

    scope 개념 메서드든 함수든 무조건 선언된 함수의 scope안에서 Scopes의 값이 정해진다

--- js/14.weather.js
+++ js/14.weather.js
@@ -1,83 +1,89 @@
 // 7f65332e6d33816b773f35daec5e2d44
-// https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=7f65332e6d33816b773f35daec5e2d44&units=metric
-// https://openweathermap.org/img/wn/10d@2x.png
+// https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=02efdd64bdc14b279bc91d9247db4722&units=metric
+// http://openweathermap.org/img/wn/10d@2x.png
 
-// ? 문제 1. ../json/city.list.json의 모든 데이터를 cityList:Array에 담으시오.
-// ? .ajax, axios, XMLHttpRequest 셋 중에 하나를 선택하시오.
-// ? 문제 2. 가져온 데이터에서 한국의 정보만을 korCity:Array에 담으시오.
-// ? 문제 3. korCity에서 {도시이름 name, 위도 lat, 경도 lon, 아이디 id} 객체를 city:Array에 담으시오. 
-/* 
-var cityList = [];
-// axios
-axios.get('../json/city.list.json').then(onResult);
+// ? 문제 1. ../json/city.list.json의 모든 데이터를 cityList: Array에 담으시오.
+// ? ($.get, axios, XMLHttpRequest 셋중의 하나를 선택하시오)
+// ? 문제 2 .가져온 데이터에서 한국의 정보만을 korCity: Array에 담으시오.
+// ? 문제 3. korCity에서 { 도시이름 city, 위도 lat, 경도 lon, id } 객체를 city: Array에 담으시오.
 
-function onResult(v) {
-	for(var i = 0; i < v.data.length; i++) {
-		cityList.push(v.data[i]);
-	}
-	var korCity;
-	korCity = cityList.filter(function(v) {
-		return v.country === "KR";
-	});
-	console.log(korCity)
-	var city = korCity.map(function(v) {
-		v.city = {name: v.name, lat: v.coord.lat , lon: v.coord.lon, id: v.id}
-		return v.city
-	}) 
-	console.log(city);
+/*
+axios.get('../json/city.list.json').then(onGet).catch(onError);
+function onGet(r) {
+	const cityList = r.data;
+	const korCity = cityList.filter(v => v.country === 'KR');
+	const city = korCity.map(v => ({ id: v.id, name: v.name, lat: v.coord.lat, lon: v.coord.lon }));
+	const seoul = city.filter(v => v.name === 'Seoul');
+	let [, { id, name, lat, lon }] = seoul;
+	console.log(korCity, city);
+	console.log(id, name, lat, lon);
+	// console.log( JSON.stringify(city) );
 }
- */
+function onError(err) {
+	console.log(err);
+}
+
+axios.get('../json/city.list.json').then(onGet).catch(onError);
+function onGet(res) {
+	const korList = [];
+	const city = [];
+	const cityList = res.data;
+	for(let v of res.data) {
+		if(v.country === 'KR') korList.push(v);
+	}
+	for(let v of korList) {
+		city.push({ id: v.id, name: v.name, lat: v.coord.lat, lon: v.coord.lon });
+	}
+	console.log(korList, city);
+}
+*/
+
+// navigator.clipboard.writeText("<Hello World>").then(() => { console.log('카피되었습니다.') })
 
 navigator.geolocation.getCurrentPosition(onCoordSuccess, onCoordError);
-
 function onCoordSuccess(r) {
-	let {latitude: lat, longitude: lon} = r.coords;
-	console.log(lat, lon)
+	let { latitude: lat, longitude: lon } = r.coords;
+	console.log(lat, lon);
 }
-
 function onCoordError(err) {
 	// 1835848
 	console.log(err);
 }
 
-// navigator.clipboard.writeText("<Hello World>").then(() => {
-// 	console.log('카피되었습니다.');
-// })
 
-/************* Global init ***************/
+
+
+
+/*************** global init **************/
 const appid = '7f65332e6d33816b773f35daec5e2d44';
-const url = 'https://api.openweathermap.org/data/2.5/weather';
-const icons =  ['https://openweathermap.org/img/wn/', '@2x.png'];
-const param = {units: 'metric', appid};
+const url = 'http://127.0.0.1:3000/weather';
+const icons = ['http://openweathermap.org/img/wn/', '@2x.png'];
+const param = { units: 'metric', appid };
 
-
-
-/************* user function *************/
+/************** user function *************/
 function init() {
 	navigator.geolocation.getCurrentPosition(onCoordSuccess, onCoordError);
-
 	function onCoordSuccess(r) {
-	let {latitude: lat, longitude: lon} = r.coords;
-	axios.get(url, {params: {...param, lat, lon} }).then(onGetWeather).catch(onError);
+		let { latitude: lat, longitude: lon } = r.coords;
+		axios.get(url, { params: { ...param, lat, lon } }).then(onGetWeather).catch(onError);
 	}
-
 	function onCoordError(err) {
-		axios.get(url, {params: {...param, id: '1835848'} }).then(onGetWeather).catch(onError);
+		axios.get(url, { params: { ...param, id: '1835848' } }).then(onGetWeather).catch(onError);
 	}
 }
 
-/************* event callback ************/
+/************** event callback ************/
 function onGetWeather(r) {
-	console.log(r);
-	let {main, name, weather} = r.data;
-	let {temp} = main;
-	// let [{main: title, description, icon}] = weather; // 이렇게도 구조분해 할당을 할 수 있다. check
-	let {main: title, description, icon} = weather[0];
+	console.log(r)
+	console.log(r.data)
+	let { main, name, weather } = r.data;
+	let { temp } = main;
+	let { main: title, description, icon } = weather[0];
+	let html = `<img src="${icons[0] + icon + icons[1]}" alt="${icon}" class="w100">`;
 	const $wrap = $('.weather-wrap');
 	$wrap.find('.city span').text(name);
-	let html = `<img src="${icons[0]+icon+icons[1]}" alt="${icon}" class="w100">`;
 	$wrap.find('.img-wp').html(html);
-	//$wrap.find('.img-wp img').attr('src', icons[0]+icon+icons[1]);
+	// $wrap.find('.img-wp img').attr('src', icons[0] + icon + icons[1]);
 	$wrap.find('.temp-wp span').text(temp);
 	$wrap.find('.desc-wp .main').text(title);
 	$wrap.find('.desc-wp .description').text(description);
@@ -87,9 +93,9 @@ function onError(err) {
 	console.log(err);
 }
 
-/************* event init ****************/
+/*************** event init ***************/
 
 
-/************* start init ****************/
+/*************** start init ***************/
 init();
 
